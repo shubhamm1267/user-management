b8e92c2ea3dd7d3fcff8250ce205e56788d4232a
shubhamm1267 <133372264+shubhamm1267@users.noreply.github.com>
2024-01-19 14:21:21 +0530

    Delete youtube-downloader/youtube-downloader directory

--- youtube-downloader/youtube-downloader/dist/youtube-downloader/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-PFMJZGKQ.mjs";import"./chunk-JHEPPOBB.mjs";import"./chunk-CQQYDMVL.mjs";import"./chunk-SQTYDHOM.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
